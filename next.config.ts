935bc15690d272e95f7cf33e5ee2c7a3c1422bb3
chriskang <kangchenhe666@gmail.com>
2026-01-24 23:38:25 +0800

    chore(config): optimize next.config and update gitignore

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'grainy-gradients.vercel.app',
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ["@phosphor-icons/react"],
+  },
 };
 
 export default nextConfig;
